23b7276f6061402dfcee26b2b563d4b645cab4b2
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:47:04 -0300

    Update streamlit_app.py
    Inserido multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ st.caption("Omega 3 & Blueberry Oatmeal")
 st.caption("Kale, Spinach & Rocket Smoothie")
 st.caption("Hard-Boiled Free-Range Egg")
 
+# Display the table on the page.
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page.
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
 
 
