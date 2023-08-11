21e5ea95df653ec105ccf54f215ca0fa27942321
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:45:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.caption("Kale, Spinach & Rocket Smoothie")
 st.caption("Hard-Boiled Free-Range Egg")
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
 
 
 
