86b6254b15c10d1f0abf76d2b783b82f686915bf
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:23:52 -0300

    Update streamlit_app.py
    Inclusão de menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("My Parents New Healthy Dinner")
+st.text("Breakfast Menu")
+st.text("Omega 3 & Blueberry Oatmeal")
+st.text("Kale, Spinach & Rocket Smoothie")
+st.text("Hard-Boiled Free-Range Egg")
+
+
