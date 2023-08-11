7d841e8af5511de3f4de489d19c223b4104163fe
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:34:07 -0300

    Update streamlit_app.py
    Inserido ícones

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.title("My Parents New Healthy Dinner")
-st.header("Breakfast Menu")
-st.caption("Omega 3 & Blueberry Oatmeal")
-st.caption("Kale, Spinach & Rocket Smoothie")
-st.caption("Hard-Boiled Free-Range Egg")
+st.header("Breakfast Favorite")
+st.caption(icon='knife_fork_plate',"Omega 3 & Blueberry Oatmeal")
+st.caption(icon='green_salad',"Kale, Spinach & Rocket Smoothie")
+st.caption(icon='chicken',"Hard-Boiled Free-Range Egg")
+st.caption(icon='avocado',"Hard-Boiled Free-Range Egg")
 
 
