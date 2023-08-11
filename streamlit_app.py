af05f4b03b0077b53d34fa4e2238b2ec7999de21
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:25:38 -0300

    Update streamlit_app.py
    Inclusão de comandos de texto

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-import streamlit as st
+erimport streamlit as st
 
 st.title("My Parents New Healthy Dinner")
-st.text("Breakfast Menu")
-st.text("Omega 3 & Blueberry Oatmeal")
-st.text("Kale, Spinach & Rocket Smoothie")
-st.text("Hard-Boiled Free-Range Egg")
+st.header("Breakfast Menu")
+st.caption("Omega 3 & Blueberry Oatmeal")
+st.caption("Kale, Spinach & Rocket Smoothie")
+st.caption("Hard-Boiled Free-Range Egg")
 
 
