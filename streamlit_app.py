53035c7954b4a484e09109779c428d1e8fad8698
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:35:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("My Parents New Healthy Dinner")
 st.header("Breakfast Favorite")
-st.caption(icon='knife_fork_plate',"Omega 3 & Blueberry Oatmeal")
+st.caption"Omega 3 & Blueberry Oatmeal", (icon='knife_fork_plate')
 st.caption(icon='green_salad',"Kale, Spinach & Rocket Smoothie")
 st.caption(icon='chicken',"Hard-Boiled Free-Range Egg")
 st.caption(icon='avocado',"Hard-Boiled Free-Range Egg")
