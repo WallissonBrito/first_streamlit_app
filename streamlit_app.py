1cb79989f33a9ecbdca96b0ef65fb4271321edc3
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-11 15:43:36 -0300

    Update streamlit_app.py
    Inserido dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My Parents New Healthy Dinner")
 st.header("Breakfast Favorite")
 st.caption("Omega 3 & Blueberry Oatmeal")
 st.caption("Kale, Spinach & Rocket Smoothie")
 st.caption("Hard-Boiled Free-Range Egg")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
 
