e98e71e2114d191d8e2d05e0713f66dccb8e7085
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-16 10:17:01 -0300

    Update streamlit_app.py
    Input para informar a fruta que busca informações

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 st.title("My Parents New Healthy Dinner")
 st.header("Breakfast Favorite")
 st.caption("Omega 3 & Blueberry Oatmeal")
@@ -21,6 +19,13 @@ st.dataframe(Fruits_to_show)
 
 # Nova seção para mostrar fruityvice api response
 st.header("Fruityvice Fruit Advice!")
+
+# Input da fruta para obter informações
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+# Request da api
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Normalizando dados 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
