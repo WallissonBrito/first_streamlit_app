3f5adfccb9d2fb37ebb9eac67c0fcda8bd638510
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-16 10:05:58 -0300

    Update streamlit_app.py
    Normalizando dados json

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ st.caption("Omega 3 & Blueberry Oatmeal")
 st.caption("Kale, Spinach & Rocket Smoothie")
 st.caption("Hard-Boiled Free-Range Egg")
 
-# Display the table on the page.
+# Mostra tabela na página
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Lista de frutas selecionadas
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 Fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(Fruits_to_show)
@@ -24,5 +24,9 @@ st.header("Fruityvice Fruit Advice!")
 
 st.text(fruityvice_response.json())
 
+#Normalizando dados 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Mostrar dados em formato de tabela
+st.dataframe(fruityvice_normalized)
 
 
