a53f8980277cdeed7a58695a61ffc46f776efe36
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-17 10:31:51 -0300

    Update streamlit_app.py
    Conector - dados snowflake (user, account e region)

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import pandas as pd
 import requests 
 import snowflake.connector
 
+#  Conector snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+
 st.title("My Parents New Healthy Dinner")
 st.header("Breakfast Favorite")
 st.caption("Omega 3 & Blueberry Oatmeal")
@@ -32,5 +38,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Mostrar dados em formato de tabela
 st.dataframe(fruityvice_normalized)
+
+#  Texto conector snowflake
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 
