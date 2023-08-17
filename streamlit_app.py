b4d060210d20b0593de86b50ec6a6d9725ba3379
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-17 11:16:23 -0300

    Update streamlit_app.py
    Inclusão de select * from fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,15 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
+
+my_cur2 = my_cnx.cursor()
+my_cur2.execute("SELECT * FROM fruit_load_list")
+my_data_row2 = my_cur2.fetchone()
+
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row2)
 
 
