cd5cad71818a37e33938c31e12bc4f1c555980b5
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-17 10:53:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,16 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Lista de frutas selecionadas
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 Fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(Fruits_to_show)
+streamlit.dataframe(Fruits_to_show)
 
 # Nova seção para mostrar fruityvice api response
-st.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
 # Input da fruta para obter informações
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Request da api
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -31,14 +31,14 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #Normalizando dados 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Mostrar dados em formato de tabela
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 minha_cur = minha_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Olá do floco de neve:")
-st.text(my_data_row)
+streamlit.text("Olá do floco de neve:")
+streamlit.text(my_data_row)
 
 
 
