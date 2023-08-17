9ba32bcabdad3522e051e1da03108db2203367cf
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-17 13:56:12 -0300

    Update streamlit_app.py
    Inclusão de botão e função get_fruit_load_list():

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,19 @@ def get_fruityvice_data(this_fruit_choice):
 
 # Nova seção para mostrar fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM fruit_load_list")
+    return my_cur.fetchall()
+
+#  Adicionando um botão
+if streamlit.button('Get Fruit Load List'):
+  #  Conexão com snowflake
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+
 try:
   # Input da fruta para obter informações
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -48,17 +61,13 @@ except URLError as e:
 #  Não executa nada após aqui 
 streamlit.stop()
 
-#  Conexão com snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 
-#  Comando query para seleção 
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchall()
+
+
 
 #  Exibir lista de frutas
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+
 
 # Inserir fruta na lista
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
