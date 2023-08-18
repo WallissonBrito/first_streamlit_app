6ae32e307a91109718d71a4ef91efecb62e40baf
Wallisson Brito <100791645+WallissonBrito@users.noreply.github.com>
2023-08-18 12:01:44 -0300

    Update streamlit_app.py
    Correção nome variável

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ if streamlit.button('Get Fruit Load List'):
 
 
 #  Função para inserir linhas pelo botão
-def insert_row_snorflake(new_fruit):
+def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding " + new_fruit
@@ -71,8 +71,8 @@ def insert_row_snorflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_finction = insert_row_snowflake(add_my_first)
-  streamlit.text(back_from_finction)
+  back_from_function = insert_row_snowflake(add_my_first)
+  streamlit.text(back_from_function)
   streamlit.write('Thanks for adding', add_my_fruit)
 
 
